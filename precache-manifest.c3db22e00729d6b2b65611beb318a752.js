b06ee27bd1135dff2cc0ed2ac9ebb5f1ae23d901
agkosd <sonawaneakshay039@gmail.com>
2020-11-14 18:59:07 +1100

    Updates

---
+++ precache-manifest.c3db22e00729d6b2b65611beb318a752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68ec8776698abbcdd6bba45cb95494d",
+    "url": "/iborrow-deploy-temp/index.html"
+  },
+  {
+    "revision": "49061fc57b9b3e7c0a81",
+    "url": "/iborrow-deploy-temp/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "49061fc57b9b3e7c0a81",
+    "url": "/iborrow-deploy-temp/static/js/2.ce249f2b.chunk.js"
+  },
+  {
+    "revision": "1cb7dc2780a48ae141ab0b774ad9fa90",
+    "url": "/iborrow-deploy-temp/static/js/2.ce249f2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a831946b29a764dfe4",
+    "url": "/iborrow-deploy-temp/static/js/main.6aa73eaf.chunk.js"
+  },
+  {
+    "revision": "b0928c8697d4db8fbfe1",
+    "url": "/iborrow-deploy-temp/static/js/runtime-main.4ef69077.js"
+  }
+]);
